b5abbcd71bcb0b151bdfa9edef3adf39e829252a
Daman <daman.kinra@gmail.com>
2020-09-21 23:26:15 +0530

    Done

--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Hero.css';
 
 function Hero({handleLogout}) {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='cont'>
+        <div className='nav'>
+            <h1>XcLass</h1>
             <button onClick={handleLogout}>Logout</button>
+        </div>
         </div>
     )
 }
--- src/Login.js
+++ src/Login.js
@@ -1,30 +1,45 @@
 import React from 'react'
+import './Login.css'
 
 function Login(props) {
     return (
         <div className="login-cont">
+            <div className="Inner-cont">
+                <div className='heading'>
+                    {props.pre? (
+                        <h1><strong>Sign-In</strong></h1>
+                    ):(
+                        <h1><strong>Sign-Up</strong></h1>
+                    )}
+                </div>
+            <p className='errors'>{props.emailError}</p>
             <input type="text"
             value={props.email}
             onChange={e=> props.setEmail(e.target.value)}
+            className='input'
+            placeholder='Email...'
             />
-            <p>{props.emailError}</p>
+            <p className='errors'>{props.passError}</p>
             <input type="password"
             value={props.pass}
             onChange={e=> props.setPass(e.target.value)}
+            className='input'
+            placeholder='Password...'
             />
-            <p>{props.passError}</p>
-            <div className='btn'>
+            
+            <div className='buttons'>
                 {props.pre ? (
                     <>
-                    <button onClick={props.handleLogin}>SignIn</button>
-                    <p>Dont have Acc <span onClick={()=> props.setPre(!props.pre)}>signup</span></p>
+                    <button onClick={props.handleLogin} className='btn'>Sign-in</button>
+                    <p>Register?<span onClick={()=> props.setPre(!props.pre)}>Sign-up</span></p>
                     </>
                 ):(
                     <>
-                    <button onClick={props.signUp}>SignUp</button>
-                    <p>Have An Acc <span onClick={()=>props.setPre(!props.pre)}>signin</span></p>
+                    <button onClick={props.signUp} className='btn'>Sign-up</button>
+                    <p>Have an account?<span onClick={()=>props.setPre(!props.pre)}>Sign-in</span></p>
                     </>
                 )}
+            </div>
             </div>
         </div>
     )
